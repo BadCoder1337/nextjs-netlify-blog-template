f479e79dfda5151843fde56173a54f5237029bb4
BadCoder1337 <anton.tiz.musin@gmail.com>
2021-09-23 22:48:12 +0300

    Landing art

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
+import tw from "tailwind-styled-components";
 import Layout from "../components/Layout";
 import BasicMeta from "../components/meta/BasicMeta";
 import OpenGraphMeta from "../components/meta/OpenGraphMeta";
 import TwitterCardMeta from "../components/meta/TwitterCardMeta";
 import { SocialList } from "../components/SocialList";
+
+const ArtContainer = tw.div`
+  bg-operators-art
+  flex
+  items-center
+  justify-center
+`
 
 export default function Index() {
   return (
@@ -10,6 +18,12 @@ export default function Index() {
       <BasicMeta url={"/"} />
       <OpenGraphMeta url={"/"} />
       <TwitterCardMeta url={"/"} />
+      <ArtContainer>
+        <div className="backdrop-filter backdrop-blur-md p-3 rounded-xl">
+          <h1 className="text-5xl">Rainbow6-RUSSIA</h1>
+          <h2 className="text-2xl">Не просто Discord, а самое большое и уютное сообщество по Rainbow Six Siege</h2>
+        </div>
+      </ArtContainer>
       <div className="container text-shadow-xl">
         <div>
           <h1>
@@ -20,44 +34,6 @@ export default function Index() {
           {/* <SocialList /> */}
         </div>
       </div>
-      <style jsx>{`
-        .container {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex: 1 1 auto;
-          padding: 0 1.5rem;
-          height: 120vh;
-        }
-        h1 {
-          font-size: 2.5rem;
-          margin: 0;
-          font-weight: 500;
-        }
-        h2 {
-          font-size: 1.75rem;
-          font-weight: 400;
-          line-height: 1.25;
-        }
-        .fancy {
-          color: #15847d;
-        }
-        .handle {
-          display: inline-block;
-          margin-top: 0.275em;
-          color: #9b9b9b;
-          letter-spacing: 0.05em;
-        }
-
-        @media (min-width: 769px) {
-          h1 {
-            font-size: 3rem;
-          }
-          h2 {
-            font-size: 2.25rem;
-          }
-        }
-      `}</style>
     </Layout>
   );
 }
